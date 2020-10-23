1dbd02c966a2e83ef99f9770028d547e3a18c8ea
XcenaX <vlad-057@mail.ru>
2020-10-23 14:54:09 +0600

    static root

--- market/market/settings.py
+++ market/market/settings.py
@@ -14,7 +14,7 @@ from pathlib import Path
 import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
 # Quick-start development settings - unsuitable for production
@@ -130,8 +130,10 @@ STAR_RATINGS_RERATE = False
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 #STATIC_ROOT = '/home/digitalportfolio/Digital-Portfolio/main/static'
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
-STATIC_ROOT = '/home/c/cu78443/public_html/Cosmetics-Shop/market/main/static'
+
 EMAIL_HOST = "smtp.gmail.com"
 EMAIL_HOST_USER = "digital.portfolio.ru@gmail.com"
 EMAIL_HOST_PASSWORD = 'Dagad583#'
